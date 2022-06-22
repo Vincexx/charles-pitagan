a94cf99db062de054e69e1dca366d5b2eadfbb4a
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-06-22 11:54:45 +0800

    added loading effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import "./App.css";
 import Content from "./components/Content";
 import Showcase from "./components/Showcase";
+import HashLoader from "react-spinners/HashLoader";
+import { useEffect, useState } from "react";
 
 function App() {
-  const primary_color = "text-cyan-500";
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 4000);
+  }, []);
+
   return (
     <>
       <div class="h-screen md:flex">
-        <Showcase primary_color={primary_color} />
-        <Content primary_color={primary_color} />
+        {loading ? (
+          <div className="w-full h-full bg-bg flex justify-center items-center">
+            <HashLoader color={"#62F7D6"} loading={loading} size={75} />
+          </div>
+        ) : (
+          <>
+            <Showcase />
+            <Content />
+          </>
+        )}
       </div>
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-card text-center py-6 mx-2">
-      <p className="text-title text-sm uppercase">
+      <p className="text-sub text-sm uppercase">
         Copyright 2020 &copy; Charles Pitagan
       </p>
     </div>
