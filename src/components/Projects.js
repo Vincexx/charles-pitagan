2562738684a7b6adae19c205a7f3853332611c01
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-08-30 12:38:44 +0800

    added overlay

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,28 +2,29 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faProjectDiagram } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
+import { FaGithub, FaLink } from "react-icons/fa";
 
 const Projects = () => {
   const data = [
     {
       link: "https://vincexx.github.io/React-Movie-App/",
       img: "/img/projects/movie.PNG",
-      github: "",
+      github: "https://github.com/Vincexx/React-Movie-App",
     },
     {
       link: "https://www.youtube.com/watch?v=jrIDlcemUso",
       img: "/img/projects/blog.png",
-      github: "",
+      github: "https://github.com/Vincexx/VnxeBlog",
     },
     {
       link: "https://vincexx.github.io/medtech/",
       img: "/img/projects/medtech.PNG",
-      github: "",
+      github: "https://github.com/Vincexx/medtech",
     },
     {
       link: "https://vincexx.github.io/growthUpsExam/",
       img: "/img/projects/growthups.PNG",
-      github: "",
+      github: "https://github.com/Vincexx/growthUpsExam",
     },
   ];
 
@@ -47,13 +48,21 @@ const Projects = () => {
         </div>
         <div className="grid md:grid-cols-2 gap-2 justify-center">
           {data.map((item) => (
-            <a href={item.link} target="__blank">
-              <img
-                className="transform transition duration-500 hover:scale-110 grayscale hover:grayscale-0 w-full"
-                src={process.env.PUBLIC_URL + item.img}
-                alt="movie"
-              ></img>
-            </a>
+            <div className="relative">
+              <div className="group absolute w-full h-full flex items-center justify-center hover:bg-primary transition-all duration-500">
+                <div className="hidden group-hover:block">
+                  <div className="flex">
+                    <a href={item.link} target="__blank">
+                      <FaLink className="text-xl hover:scale-110" />
+                    </a>
+                    <a href={item.github} target="__blank">
+                      <FaGithub className="text-xl ml-1 hover:scale-110" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <img src={process.env.PUBLIC_URL + item.img} alt="movie"></img>
+            </div>
           ))}
         </div>
 
