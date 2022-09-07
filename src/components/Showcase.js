2aee2a4303af65dbd4f7db875d91b812124d597a
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-09-07 12:00:04 +0800

    fixed design on showcase

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faMobileAndroid } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
-import About from "./About";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
@@ -16,10 +15,6 @@ const Showcase = () => {
     "
       >
         <div className="container mx-auto md:px-8 relative">
-          <div className="py-4 mx-2">
-            <p className={`font-extrabold text-sm text-primary`}>FUTURESRDEV</p>
-          </div>
-
           <motion.div
             initial={{ x: -300, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
@@ -27,33 +22,41 @@ const Showcase = () => {
               y: { type: "tween", stiffness: 100 },
               default: { duration: 1 },
             }}
-            className="bg-card mx-2 md:px-3 md:pt-8 pt-6 mb-6 shadow-lg"
+            className="bg-card mx-2 md:px-3 md:pt-8 pt-6 shadow-lg md:h-screen flex flex-col justify-between"
           >
-            <div className="text-center">
-              <span>
-                <h2>--</h2>
-                <TypeAnimation
-                  cursor={true}
-                  sequence={["Hi, I am", 4000, ""]}
-                  wrapper="h2"
-                  repeat={Infinity}
-                />
-              </span>
-              <h1 className={`font-bold md:text-5xl text-2xl text-primary`}>
-                Charles Pitagan{" "}
-              </h1>
-              <p className="my-1 text-md text-title font-bold">
-                Software Engineer
-              </p>
-              <p className="text-desc text-sm">
-                "The expert in anything was once a beginner"
+            <div className="mx-2">
+              <p className={`font-extrabold text-sm text-primary`}>
+                FUTURESRDEV
               </p>
             </div>
-            <div className="grid grid-cols-1 justify-items-center">
-              <img
-                src={process.env.PUBLIC_URL + "/img/profile.png"}
-                alt="profile"
-              ></img>
+
+            <div>
+              <div className="text-center">
+                <span>
+                  <h2>--</h2>
+                  <TypeAnimation
+                    cursor={true}
+                    sequence={["Hi, I am", 4000, ""]}
+                    wrapper="h2"
+                    repeat={Infinity}
+                  />
+                </span>
+                <h1 className={`font-bold md:text-5xl text-2xl text-primary`}>
+                  Charles Pitagan{" "}
+                </h1>
+                <p className="my-1 text-md text-title font-bold">
+                  Software Engineer
+                </p>
+                <p className="text-desc text-sm">
+                  "The expert in anything was once a beginner"
+                </p>
+              </div>
+              <div className="grid grid-cols-1 justify-items-center">
+                <img
+                  src={process.env.PUBLIC_URL + "/img/profile.png"}
+                  alt="profile"
+                ></img>
+              </div>
             </div>
 
             <div className="flex justify-between md:mt-3 mt-12 px-4 pb-4 items-center">
@@ -86,8 +89,6 @@ const Showcase = () => {
               </div>
             </div>
           </motion.div>
-
-          <About />
         </div>
       </div>
     </>
